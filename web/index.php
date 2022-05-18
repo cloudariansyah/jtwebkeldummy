ae59f573bf25f9b32da53c6e514241d99373827a
cloud ariansyahcenter <65204447+cloudariansyah@users.noreply.github.com>
2022-05-18 09:41:26 +0700

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Heroic Features - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+		<link href="css/carousel.css" rel="stylesheet" />
+		
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="js/bootstrap.min.css" rel="stylesheet">
+
+   
+		
+		
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/holder.min.js"></script>
+		<script src="js/jquery-3.2.1.slim.min.js"></script>
+		<script src="js/popper.min.js"></script>
+    </head>
+    <body>
+     
+		 <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#!">Kelurahan Pinang Ranti</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                      <!--  <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
+                    -->
+					</ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+		 <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+		 
+		  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Example headline.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Another example headline.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>One more for good measure.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h4 class="font-weight-light">Kecamatan Makasar</h4>
+					<h1 class="font-weight-light">Kelurahan Pinang Ranti</h1>
+					<p>terletak di Jl. SMA 48 RT 14 RW 01 Kelurahan Pinang Ranti Kecamatan Makasar – 13560 -  Jakarta Timur luas wilayah 235,15 Ha dengan jumlah KK XX jumlah Penduduk XX RW 5 jumlah RW 54.</p>
+                    <a class="btn btn-primary" href="#!">Call Center</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31726.481519493445!2d106.86785538411829!3d-6.288662051087423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f299b21a9357%3A0x8451f94f09d2490b!2sPinang%20Ranti%2C%20Makasar%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644469039098!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Tupoksi</h2>
+                            <p class="card-text">
+							<ul><li>Pelayanan Pencatatan Biodata Penduduk WNI</li>
+<li>Pelayanan Penertiban KK</li>
+<li>Pelayanan Biodata Penduduk WNI yang ebrtempat tinggal lama di DKI Jakarta dan belum terdaftar dalam database kependudukan/belum</li> <li>memiliki NIK</li>
+<li>Penerbitan KK karena perubahan data</li>
+<li>Pelayanan KK karena hilang atau rusak </li>
+<li>Pelayanan KTP-el karena adanya perubahan data/biodata</li>
+<li>Pelayanan KTP-el karena hilang atau rusak</li>
+<li>Pelayanan KTP-el bagi Penduduk WNI di luar domisili</li>
+<!--<li>Pelayanan perekaman KTP-el baru</li>
+<li>Pelayanan KTP-el karena pindah data</li>
+<li>Pelayanan SKP</li>
+<li>Pelayanan SKP penduduk yang dilakukan di Kelurahan tujuan bersamaan dengan pelaporan kedatangan</li>
+<li>Pelayanan pelaporan kedatangan penduduk</li>
+<li>Pencatatan kelahiran penduduk WNI di wilayah RI</li>
+<li>Pencatatan kematian WNI</li>
+<li>Pencatatan kelahiran penduduk WNI yang tidak diketahui asal usulnya atau tidak diketahui keberadaan orang tuanya</li>
+<li>Pencatatan kematian seseorang yang tidak jelas identitasnya</li>
+<li>Pencatatan kematian penduduk yang hilang dan tidak ditemukan jenazahnya atau tidak jelas keberadaannya</li>
+<li>Pelayanan KIA</li>
+<li>Pelayanan KIA karena hilang atau rusak</li>
+<li>Pebdataan penduduk Non Permanen Pelaksanaan pendataan penduduk nonpermanen oleh penduduk nonpermanen di kelurahan</li>-->
+</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Selengkapnya</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Potensi Wilayah</h2>
+                            <p class="card-text">
+							<ul>
+							<li>Rumah Sakit Haji Jakarta</li>
+<li>RS TK.II Moh. Ridwan Meuraksa</li>
+<li>Puskesmas Pinang Ranti</li>
+<li>Klinik Taradita 48</li>
+<li>Apotik Yeta Farma II</li>
+<li>Mall Tamini Square</li>
+<li>Restorant Tradisional KARIMATA</li>
+<li>Kuliner Makanan JCO</li>
+<li>Holland Bakery</li>
+<li>D'cika Cakes & Bakery</li>
+<li>Pom Bensin Pertamina</li>
+<li>Taman Anggrek Indonesia Permai</li>
+<li>Pizza Hut Restoran</li>
+</p>
+                        </div>
+                        <!--<div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>-->
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Hubungi</h2>
+                            <p class="card-text">Jam operasional 07.00-16.00 WIB</p>
+							<ul>
+							<li>TikTok: </li>
+							<li>Twitter: </li>
+							<li>Instagram: <a href="https://instagram.com/kelpinangranti">@kelpinangranti</a> </li>
+							<li>Facebook: </li>
+							<li>Youtube: </li>
+							</ul>
+                        </div>
+                       <!-- <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Content-->
+		
+       
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+		
+		
+    </body>
+</html>
