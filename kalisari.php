78e8dd5a70f42c2e688136cc5fd3fce8aeb06fc2
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-05-31 20:57:53 +0700

    Update and rename Kalisari.php to kalisari.php
    Mengganti nama file

--- kalisari.php
+++ kalisari.php
@@ -142,7 +142,7 @@ s0.parentNode.insertBefore(s1,s0);
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
-                    <h4 class="font-weight-light">Kelurahan Kalisari</h4>
+                    <h4 class="font-weight-light">Kecamatan Pasar Rebo</h4>
 					<h1 class="font-weight-light">Kelurahan Kalisari</h1>
 					<p>terletak di Jl. Kalisari RT 011/02 No.Raya, Kalisari, Kec. Ps. Rebo, Kota Jakarta Timur</p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
@@ -178,7 +178,7 @@ s0.parentNode.insertBefore(s1,s0);
         <div class="row featurette">
 		
           <div class="col-md-7">
-            <h2 class="featurette-heading">Kelurahan Kalisari<span class="text-muted">Kelurahan Kalisari</span></h2>
+            <h2 class="featurette-heading">Kelurahan Kalisari<span class="text-muted">Kecamatan Pasar Rebo</span></h2>
             <p class="lead">terletak di Jl. Kalisari RT 011/02 No.Raya, Kalisari, Kec. Ps. Rebo, Kota Jakarta Timur</p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
           </div>
