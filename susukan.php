b6de578437c23bc1d12ecd561502e420db663a74
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-05-31 21:50:10 +0700

    Update susukan.php
    menambahkan alamat

--- susukan.php
+++ susukan.php
@@ -144,7 +144,7 @@ s0.parentNode.insertBefore(s1,s0);
                 <div class="col-lg-5">
                     <h4 class="font-weight-light">Kecamatan Ciracas</h4>
 					<h1 class="font-weight-light">Kelurahan Susukan</h1>
-					<p>terletak di Jl. </p>
+					<p>terletak di Jl. H. Baping RT. 010 RW. 06 </p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
                 </div>
             </div>
@@ -179,7 +179,7 @@ s0.parentNode.insertBefore(s1,s0);
 		
           <div class="col-md-7">
             <h2 class="featurette-heading">Kelurahan Susukan <span class="text-muted">Kecamatan Ciracas</span></h2>
-            <p class="lead">terletak di Jl. </p>
+            <p class="lead">terletak di Jl. H. Baping RT. 010 RW. 06 </p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
           </div>
           <div class="col-md-5">
