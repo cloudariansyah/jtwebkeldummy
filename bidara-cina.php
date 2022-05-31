d7d23bedfa5957308e50e2483380014fb740091e
ummuhanis <103159148+ummuhanis@users.noreply.github.com>
2022-05-31 22:02:44 +0700

    Update bidara-cina.php
    hani mengubah alamat kel bidara cina

--- bidara-cina.php
+++ bidara-cina.php
@@ -141,7 +141,7 @@ s0.parentNode.insertBefore(s1,s0);
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
-                    <h4 class="font-weight-light">Kecamatan Makasar</h4>
+                    <h4 class="font-weight-light">Kecamatan Jatinegara</h4>
 					<h1 class="font-weight-light">Kelurahan Bidara Cina</h1>
 					<p>terletak di Jl. </p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
@@ -177,8 +177,8 @@ s0.parentNode.insertBefore(s1,s0);
         <div class="row featurette">
 		
           <div class="col-md-7">
-            <h2 class="featurette-heading">Kelurahan Bidara Cina <span class="text-muted">Kecamatan Makasar</span></h2>
-            <p class="lead">terletak di Jl.</p>
+            <h2 class="featurette-heading">Kelurahan Bidara Cina <span class="text-muted">Kecamatan Jatinegara</span></h2>
+            <p class="lead">terletak di Jl.Otto Iskandar Dinata, No. 32C, RT.10/RW.2, Bidara Cina, Kecamatan Jatinegara, Kota Jakarta Timur</p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
           </div>
           <div class="col-md-5">
