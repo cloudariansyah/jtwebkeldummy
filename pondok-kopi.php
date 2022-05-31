32cffe8d65a007b23600b59a25d1ea528c22140f
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-05-31 21:33:51 +0700

    Update pondok-kopi.php
    mengubah kecamatan

--- pondok-kopi.php
+++ pondok-kopi.php
@@ -142,7 +142,7 @@ s0.parentNode.insertBefore(s1,s0);
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
-                    <h4 class="font-weight-light">Kelurahan Pondok Kopi</h4>
+                    <h4 class="font-weight-light">Kecamatan Duren Sawit</h4>
 					<h1 class="font-weight-light">Kelurahan Pondok Kopi</h1>
 					<p>terletak di Jl. </p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
@@ -178,7 +178,7 @@ s0.parentNode.insertBefore(s1,s0);
         <div class="row featurette">
 		
           <div class="col-md-7">
-            <h2 class="featurette-heading">Kelurahan Pondok Kopi <span class="text-muted">Kecamatan Makasar</span></h2>
+            <h2 class="featurette-heading">Kelurahan Pondok Kopi <span class="text-muted">Kecamatan Duren Sawit</span></h2>
             <p class="lead">terletak di Jl. </p>
                     <a class="btn btn-primary" href="#!">Call Center</a>
           </div>
