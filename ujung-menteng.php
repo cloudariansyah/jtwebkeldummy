3e435b575c9f58517de6496a63b8e41c60d379fc
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-05-31 21:06:45 +0700

    Update and rename kelurahan ujung menteng.php to ujung-menteng.php
    Mengganti nama file

--- ujung-menteng.php
+++ ujung-menteng.php
@@ -52,7 +52,7 @@ s0.parentNode.insertBefore(s1,s0);
   <body>
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Kelurahan Pinang Ranti</a>
+        <a class="navbar-brand" href="#">Kelurahan Ujung Menteng</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
