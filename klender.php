05c7d7cf08e6b46dbab95c4da95b271e661c435e
Annas71 <103159153+Annas71@users.noreply.github.com>
2022-06-07 15:16:10 +0700

    Update klender.php

--- klender.php
+++ klender.php
@@ -171,6 +171,7 @@
           </div><!-- /.col-lg-4 -->
         </div><!-- /.row -->
 
+	      
         <!-- START THE FEATURETTES -->
 
         <hr class="featurette-divider">
@@ -180,7 +181,7 @@
           <div class="col-md-7">
             <h2 class="featurette-heading">Kelurahan Klender <span class="text-muted">Kecamatan Duren Sawit</span></h2>
             <p class="lead">terletak di Jl. .</p>
-                    
+                    <a class="btn btn-primary" href="#!">Call Center</a>
           </div>
           <div class="col-md-5">
 		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15865.346073520373!2d106.90040526843985!3d-6.2192785964561015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3547dd7fa17%3A0x9c9ee8a3639762aa!2sKlender%2C%20Duren%20Sawit%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644467098670!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
