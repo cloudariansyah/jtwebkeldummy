8099f99d4761e450d3b94d71388702f95796ded1
Annas71 <103159153+Annas71@users.noreply.github.com>
2022-06-07 11:48:26 +0700

    Update pulogebang.php

--- pulogebang.php
+++ pulogebang.php
@@ -146,7 +146,7 @@ s0.parentNode.insertBefore(s1,s0);
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
             <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2> camat Cakung </h2>
+            <h2>camat Cakung</h2>
             <p>NAMA</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div><!-- /.col-lg-4 -->
