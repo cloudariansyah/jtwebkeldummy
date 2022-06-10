6f3eebbf5552dfba6e6adc669bd41489c0b76aa3
AnwarIsnain <107239636+AnwarIsnain@users.noreply.github.com>
2022-06-10 15:22:33 +0700

    update munjul.php

--- munjul.php
+++ munjul.php
@@ -141,7 +141,7 @@
       <div class="container marketing">
 
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/kantor kelurahan munjul.jpeg" alt="..." /></div>
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/kantor kelurahan munjul.jpg" alt="..." /></div>
                 <div class="col-lg-5">
                     <h4 class="font-weight-light">Kecamatan Cipayung</h4>
 					<h1 class="font-weight-light">Kelurahan </h1>
