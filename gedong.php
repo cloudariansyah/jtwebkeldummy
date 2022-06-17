4ca41ab8e59926f6ecc172e1ba43f9fe04849213
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-06-17 10:37:34 +0700

    Update gedong.php

--- gedong.php
+++ gedong.php
@@ -128,6 +128,11 @@
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/gedong.jpg" alt="..." /></div>
                 <div class="col-lg-5">
+		    <h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light">Kadarusman Sutama, SE, SH</h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
+              </div>
                     <h4 class="font-weight-light">Kecamatan Pasar Rebo</h4>
 					<h1 class="font-weight-light">Kelurahan Gedong</h1>
 					<p>terletak di Jl.  Kelurahan Gedong Kecamatan Pasar rebo - 13760 - Jakarta Timur luas wilayah  dengan bujur barat  dan lintang selatan  serta jumlah rw jumlah rt .</p>
@@ -150,7 +155,7 @@
                      
           </div>
           <div class="col-md-5">
-		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15862.770597092012!2d106.85066126845314!3d-6.304052998952845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ed83f46aa99f%3A0xcbba6b081df74068!2sGedong%2C%20Pasar%20Rebo%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644469139236!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
+		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15862.770597092012!2d106.85066126845314!3d-6.304052998952845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ed83f46aa99f%3A0xcbba6b081df74068!2sGedong%2C%20Pasar%20Rebo%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644469139236!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
           <!--  <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">-->
           </div>
         </div>
