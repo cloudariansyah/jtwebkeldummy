4d29e954a2a328e105328e036fc32cbb33948113
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-06-17 10:20:03 +0700

    Update bidara-cina.php

--- bidara-cina.php
+++ bidara-cina.php
@@ -141,6 +141,11 @@ s0.parentNode.insertBefore(s1,s0);
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
+		    <h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light">Kadarusman Sutama, SE, SH</h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
+              </div>
                     <h4 class="font-weight-light">Kecamatan Jatinegara</h4>
 					<h1 class="font-weight-light">Kelurahan Bidara Cina</h1>
 					<p>terletak di Jl.Otto Iskandar di Nata, Kampung Melayu No. 32C, RT.10/RW.2, Bidara Cina, Kecamatan Jatinegara, Kota Jakarta Timur </p>
@@ -182,7 +187,7 @@ s0.parentNode.insertBefore(s1,s0);
                     <a class="btn btn-primary" href="#!">Call Center</a>
           </div>
           <div class="col-md-5">
-		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15864.887178626806!2d106.8577026684422!3d-6.234467646899974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3756d467dc3%3A0xd467b5a4f0a32f03!2sBidara%20Cina%2C%20Jatinegara%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644466076661!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
+		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15864.887178626806!2d106.8577026684422!3d-6.234467646899974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3756d467dc3%3A0xd467b5a4f0a32f03!2sBidara%20Cina%2C%20Jatinegara%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644466076661!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
           <!--  <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">-->
           </div>
         </div>
