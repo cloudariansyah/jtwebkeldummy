f707699af265eca6c5428b645f62aae4cab13d77
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-06-17 10:36:32 +0700

    Update dukuh.php

--- dukuh.php
+++ dukuh.php
@@ -143,6 +143,11 @@
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/Kantor Lurah Dukuh.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
+		    <h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light">Kadarusman Sutama, SE, SH</h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
+              </div>
                     <h4 class="font-weight-light">Kecamatan Kramat Jati</h4>
 					<h1 class="font-weight-light">Kelurahan Dukuh </h1>
 					<p>terletak di Jl. Dukuh V RT 007 RW 002 Kelurahan Dukuh  Kecamatan Kramat Jati – 13550 -  Jakarta Timur luas wilayah XX Ha dengan jumlah RT XX jumlah RW XX.</p>
@@ -163,7 +168,7 @@
                     
           </div>
           <div class="col-md-5">
-		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15863.061378066323!2d106.86918011845162!3d-6.294538598670268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f29cd92c54ab%3A0x3f4eedadfa6405ef!2sDukuh%2C%20Kramat%20Jati%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644468268663!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
+		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15863.061378066323!2d106.86918011845162!3d-6.294538598670268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f29cd92c54ab%3A0x3f4eedadfa6405ef!2sDukuh%2C%20Kramat%20Jati%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644468268663!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
           <!--  <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">-->
           </div>
         </div>
