400e186d37ecaae00a755d1a7f3c7a7e87d1aede
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-06-17 10:44:14 +0700

    Update and rename jatinegara kaum.php to jatinegara-kaum.php

---
+++ jatinegara-kaum.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kelurahan Jakarta Timur</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/custom.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Company - v2.2.1
+  * Template URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <!-- <h1 class="logo mr-auto"><a href="index.html"><span>+Jakarta</span></a></h1> -->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="index.html" class="logo mr-auto"><img src="assets/img/logo/logoJT.png" alt="" class="img-fluid"></a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Beranda</a></li>
+
+          <li class="drop-down active"><a href="">Profil</a>
+            <ul>
+				<li class="drop-down"><a href="#">Sejarah</a>
+                <ul>
+                  <li><a href="#">Benda Cagar Budaya</a></li>
+                  <li><a href="#">Bangunan Cagar Budaya</a></li>
+                  <li><a href="#">Situs</a></li>
+                  <li><a href="#">Kawasan Cagar Budaya</a></li>
+                  <li><a href="#">Cagar Budaya Tak Benda</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Peta & Batas Wilayah</a></li>
+              <li><a href="#">Demografi</a></li>
+              <li><a href="#">Geografi</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down"><a href="">Produk</a>
+            <ul>
+              <li><a href="#">Penghargaan</a></li>
+              <li><a href="#">Hukum</a></li>
+              <li><a href="#">Statistik</a></li>
+              <li><a href="laporan.html">Laporan</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down"><a href="">Layanan</a>
+            <ul>
+              <li><a href="#">PTSP</a></li>
+              <li><a href="#">Pajak</a></li>
+              <li><a href="#">Informasi</a></li>
+              <li><a href="#">Kanal Aduan</a></li>
+              <li><a href="#">Kantor Perangkat Kota</a></li>
+              <li><a href="#">Pelatihan</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down"><a href="">Fasilitas</a>
+            <ul>
+              <li><a href="#">Pendidikan</a></li>
+              <li><a href="#">Kesehatan</a></li>
+              <li><a href="#">Pariwisata</a></li>
+              <li><a href="#">Transportasi</a></li>
+              <li><a href="#">Pasar & UMKM</a></li>
+              <li><a href="#">Perumahan</a></li>
+              <li><a href="#">Internet Jakwifi</a></li>
+              <li><a href="#">Olahraga</a></li>
+              <li><a href="#">Taman</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down"><a href="">Mutakhir</a>
+            <ul>
+              <li><a href="#">Berita</a></li>
+              <li><a href="#">Inovasi</a></li>
+              <li><a href="#">Corona</a></li>
+              <li><a href="#">Pantau Banjir</a></li>
+              <li><a href="#">Buletin</a></li>
+              <li><a href="cctv.html">CCTV</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+	  <!-- .nav-menu -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header>
+  <!-- End Header -->
+<!-- ======= Hero Section ======= -->
+  <div class="container">
+    
+  </div>
+<!-- End Hero -->
+  <main id="main">
+   
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="container marketing">
+
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/Foto Kantor Kelurahan Jatinegara Kaum.jpeg" alt="..." /></div>
+                <div class="col-lg-5">
+			<h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light">Johan</h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('frontend/kelurahan/Foto Lurah Bali Mester.jpeg') }}" alt="foto-lurah" /></p>
+              </div>
+                    <h4 class="font-weight-light">Kecamatan Pulo Gadung </h4>
+					<h1 class="font-weight-light">Kelurahan Jatinegara Kaum</h1>
+					<p>terletak di Jl. Badarudin No. 1 RT 001/005, Jatinegara Kaum, Kelurahan Jatinegara Kaum, Kecamatan Pulo Gadung, 13250 -  Jakarta Timur luas wilayah XX Ha, dengan jumlah RW XX jumlah RT XX.</p>
+                    <!--<a class="btn btn-primary" href="#!">Call Center</a>-->
+                </div>
+            </div>
+       
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+		
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Kelurahan Bali Mester <span class="text-muted">Kecamatan Jatinegara</span></h2>
+            <p class="lead">terletak di Jatinegara Barat 2 No.19, RT.12/RW.3, Kecamatan Jatinegara, kotamadya Jakarta Timur, propinsi DKI Jakarta 13310 -  Jakarta Timur luas wilayah 0.67 Km2, dengan jumlah RW 6 jumlah RT 71.</p>
+                    
+          </div>
+          <div class="col-md-5">
+		  <div class="card-body"><p class="text-white m-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31731.861231249426!2d106.88353023406276!3d-6.199877047901843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4b0ae518c79%3A0x9bac0a12e5a1ecdc!2sJatinegara%20Kaum%2C%20Pulo%20Gadung%2C%20East%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1644468386964!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p></div>
+          <!--  <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">-->
+          </div>
+        </div>
+ 
+<div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Tupoksi</h2>
+                            <p class="card-text">
+							<ul><li>Pelayanan Pencatatan Biodata Penduduk WNI</li>
+<li>Pelayanan Penertiban KK</li>
+<li>Pelayanan Biodata Penduduk WNI yang bertempat tinggal lama di DKI Jakarta dan belum terdaftar dalam database kependudukan/belum</li> <li>memiliki NIK</li>
+<li>Penerbitan KK karena perubahan data</li>
+<li>Pelayanan KK karena hilang atau rusak </li>
+<li>Pelayanan KTP-el karena adanya perubahan data/biodata</li>
+<li>Pelayanan KTP-el karena hilang atau rusak</li>
+<li>Pelayanan KTP-el bagi Penduduk WNI di luar domisili</li>
+<li>Pelayanan perekaman KTP-el baru</li>
+<li>Pelayanan KTP-el karena pindah data</li>
+<li>Pelayanan SKP</li>
+<li>Pelayanan SKP penduduk yang dilakukan di Kelurahan tujuan bersamaan dengan pelaporan kedatangan</li>
+<li>Pelayanan pelaporan kedatangan penduduk</li>
+<li>Pencatatan kelahiran penduduk WNI di wilayah RI</li>
+<li>Pencatatan kematian WNI</li>
+<li>Pencatatan kelahiran penduduk WNI yang tidak diketahui asal usulnya atau tidak diketahui keberadaan orang tuanya</li>
+<li>Pencatatan kematian seseorang yang tidak jelas identitasnya</li>
+<li>Pencatatan kematian penduduk yang hilang dan tidak ditemukan jenazahnya atau tidak jelas keberadaannya</li>
+<li>Pelayanan KIA</li>
+<li>Pelayanan KIA karena hilang atau rusak</li>
+<li>Pendataan penduduk Non Permanen Pelaksanaan pendataan penduduk nonpermanen oleh penduduk nonpermanen di kelurahan</li>-->
+</p>
+                        </div>
+                       <!-- <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Selengkapnya</a></div>-->
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Potensi Wilayah</h2>
+                            <p class="card-text">
+							<ul>
+<li></li>
+</p>
+                        </div>
+                        <!--<div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>-->
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Hubungi 021 8191162</h2>
+                            <p class="card-text">Jam operasional 07.00-16.00 WIB</p>
+							<ul>
+								<li>Email: keljatinegarakaun@gmail.com </li>
+							<li>Instagram: <a href="https://instagram.com/kelurahan_balimester">@kelurahan_balimester</a> </li>
+							<li>Facebook: <a href="https://facebook.com/KelJatinegaraKaum">@Kel Jatinegara Kaum
+							</ul>
+                        </div>
+                       <!-- <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>-->
+                    </div>
+                </div>
+            </div>
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Jakarta Timur</h3>
+            <p>
+              Komplek Kantor Walikota Jakarta Timur <br>
+              Jakarta Timur 13940 <br>
+              Jl. Sentra Timur Pulo Gebang<br>
+              <!-- <strong>Phone:</strong> 021 - 48702407<br> -->
+              <strong>Email:</strong> kominfotik@jakarta.go.id<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Link Terkait</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">E-Kinerja</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">SIPKD</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">JDIH</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Nomor Penting</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Polisi : 110</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Ambulans : 118 dan 119</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Badan Nasional Pencarian dan Pertolongan : 115</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Posko Bencana Alam : 129</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Perusahaan Listrik Negara (PLN) : 123</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Pemadam Kebakaran : 113 atau 1131</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Nomor Darurat Telpon Seluler dan Satelit : 112</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Keracunan :  (021) 4250767 atau (021) 4227875</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Pencarian</h4>
+            <!-- <p></p> -->
+            <form action="" method="post">
+              <input type="text" name="text"><input type="submit" value="Cari">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Jakarta Timur</span></strong>Sudin Kominfotik JT
+        </div>
+        
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-youtube"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
