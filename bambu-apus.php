aef89858a64f9dd5ebd1d6f3802df744fc1523cf
aaryaanputraa <103159128+aaryaanputraa@users.noreply.github.com>
2022-06-17 10:17:46 +0700

    Update bambu-apus.php

--- bambu-apus.php
+++ bambu-apus.php
@@ -143,6 +143,11 @@
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/kelurahan_bambu_apus.jpg" alt="..." /></div>
                 <div class="col-lg-5">
+		    <h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light">Kadarusman Sutama, SE, SH</h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
+              </div>
                     <h4 class="font-weight-light">Kecamatan Cipayung</h4>
 					<h1 class="font-weight-light">Kelurahan Bambu Apus</h1>
 					<p>terletak di Jl. Mini 3, Kelurahan Bambu Apus, Kecamatan Cipayung Jakarta Timur 13890 -  Jakarta Timur luas wilayah 3.17 Km2 dengan jumlah RW 5 jumlah RT 63 .</p>
