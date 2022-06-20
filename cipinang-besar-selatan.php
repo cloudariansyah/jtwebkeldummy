15f1f115f34e3ae26c076c1e4f2e2463f2515d85
AnwarIsnain <107239636+AnwarIsnain@users.noreply.github.com>
2022-06-20 11:08:51 +0700

    update cipinang-besar-selatan.php

--- cipinang-besar-selatan.php
+++ cipinang-besar-selatan.php
@@ -142,7 +142,7 @@ s0.parentNode.insertBefore(s1,s0);
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
 		    <h4 class="font-weight-light">Lurah</h4>
-                    <h1 class="font-weight-light">Kadarusman Sutama, SE, SH</h1>
+                    <h1 class="font-weight-light">Dwi Sugiarti</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
                      url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
               </div>
