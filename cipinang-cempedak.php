80fc0fbe7b5e735f1b1c75520c066436b6011145
AnwarIsnain <107239636+AnwarIsnain@users.noreply.github.com>
2022-06-20 11:36:35 +0700

    update cipinang-cempedak.php

--- cipinang-cempedak.php
+++ cipinang-cempedak.php
@@ -144,7 +144,7 @@
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/kelurahan_cipinang_cimpedak.jpg" alt="..." /></div>
                 <div class="col-lg-5">
 		    <h4 class="font-weight-light">Lurah</h4>
-                    <h1 class="font-weight-light">Ika Yuli Rahayu</h1>
+                    <h1 class="font-weight-light">Abdul Muin</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
                      url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
               </div>
