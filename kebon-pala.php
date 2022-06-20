b5a3ac3d7473cf2af36678281d40dfa3b20c2c51
AnwarIsnain <107239636+AnwarIsnain@users.noreply.github.com>
2022-06-20 15:39:13 +0700

    update kebon-pala.php

--- kebon-pala.php
+++ kebon-pala.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Kelurahan Jakarta Timur</title>
+  <title>Kelurahan Kebon Pala</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
