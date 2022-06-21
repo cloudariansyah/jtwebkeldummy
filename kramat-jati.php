bc7dc616d407dea3a65c80bf7b6222a4ac2a54e1
sultan <106468100+Kailann@users.noreply.github.com>
2022-06-21 11:45:16 +0700

    Update kramat-jati.php
    menambahkan syntax untuk nama lurah dan fotonya

--- kramat-jati.php
+++ kramat-jati.php
@@ -141,6 +141,11 @@ s0.parentNode.insertBefore(s1,s0);
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
+		    <h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light"></h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('') }}" alt="foto-lurah" /></p>
+              </div>
                     <h4 class="font-weight-light">Kecamatan Kramat Jati</h4>
 					<h1 class="font-weight-light">Kelurahan Kramat Jati</h1>
 					<p>terletak di Jl. </p>
