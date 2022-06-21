c0f95e7fe9e94aeb730bd363f1584f037292e75d
sultan <106468100+Kailann@users.noreply.github.com>
2022-06-21 11:37:20 +0700

    Update pisangan baru.php
    menambahkan syntax untuk nama lurah dan fotonya

--- pisangan baru.php
+++ pisangan baru.php
@@ -143,6 +143,11 @@
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/Kelurahan Pisangan Baru.jpg" alt="..." /></div>
                 <div class="col-lg-5">
+		    <h4 class="font-weight-light">Lurah</h4>
+                    <h1 class="font-weight-light"></h1>
+                    <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
+                     url('') }}" alt="foto-lurah" /></p>
+              </div>
                     <h4 class="font-weight-light">Kecamatan Matraman</h4>
 					<h1 class="font-weight-light">Kelurahan Pisangan Baru</h1>
 					<p>terletak di Jl.Pisangan Baru, Pisangan Baru, Matraman, RT.8/RW.14, Pisangan Baru, Kec. Matraman, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13110.</p>
