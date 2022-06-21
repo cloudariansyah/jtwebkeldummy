51b3e69c1a99451a28fe4bb18fa38d9a7436029d
xvanaaa <106224215+xvanaaa@users.noreply.github.com>
2022-06-21 11:30:32 +0700

    Update pondok-ranggon.php

--- pondok-ranggon.php
+++ pondok-ranggon.php
@@ -64,7 +64,7 @@ s0.parentNode.insertBefore(s1,s0);
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
             </li>
             <!--<li class="nav-item">
               <a class="nav-link" href="#">Link</a>
