966432be08e84d4db9a09f1e7a607e1b4fe6cace
Annas71 <103159153+Annas71@users.noreply.github.com>
2022-06-21 12:37:55 +0700

    Update utan-kayu-utara.php
    Mengubah sintax

--- utan-kayu-utara.php
+++ utan-kayu-utara.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Kelurahan Utan Kayu Utarar</title>
+  <title>Kelurahan Utan Kayu Utara</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
