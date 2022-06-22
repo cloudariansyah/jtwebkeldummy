8dc608ebf19404cb282928fc3eeed527dff533b6
AnwarIsnain <107239636+AnwarIsnain@users.noreply.github.com>
2022-06-22 15:20:50 +0700

    update utan-kayu-utara.php

--- utan-kayu-utara.php
+++ utan-kayu-utara.php
@@ -144,7 +144,7 @@
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/IMG-20220609-WA0002.jpg" alt="..." /></div>
                 <div class="col-lg-5">
 		    <h4 class="font-weight-light">Lurah</h4>
-                    <h1 class="font-weight-light">Kadarusman Sutama, SE, SH</h1>
+                    <h1 class="font-weight-light">Euis Saadah Hernawati</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
                      url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
               </div>
