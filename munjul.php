e8e5bb7ec9cf1290ccb89c49ac878c4c9940a662
Fiki Pratama <107240699+fikipratama@users.noreply.github.com>
2022-06-22 13:55:06 +0700

    Update munjul.php

--- munjul.php
+++ munjul.php
@@ -144,7 +144,7 @@
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/kantor kelurahan munjul.jpg" alt="..." /></div>
                 <div class="col-lg-5">
                     <h4 class="font-weight-light">Lurah</h4>
-                    <h1 class="font-weight-light">-</h1>
+                    <h1 class="font-weight-light">Henny Hermayani</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
                      url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
               </div>
