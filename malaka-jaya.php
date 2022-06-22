2cfe54787e9f3c76d0b0391d078782af40a2987c
Fiki Pratama <107240699+fikipratama@users.noreply.github.com>
2022-06-22 13:33:48 +0700

    Update malaka-jaya.php

--- malaka-jaya.php
+++ malaka-jaya.php
@@ -139,10 +139,10 @@ s0.parentNode.insertBefore(s1,s0);
       <div class="container marketing">
 
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/halaman1.jpeg" alt="..." /></div>
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="foto/kelurahan Malakah_jaya.jpg" alt="..." /></div>
                 <div class="col-lg-5">
 			  <h4 class="font-weight-light">Lurah</h4>
-                    <h1 class="font-weight-light">-</h1>
+                    <h1 class="font-weight-light">Ngaiman</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
                      url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
               </div>
