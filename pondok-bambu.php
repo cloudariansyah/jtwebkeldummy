ee7064a90d6801e4fd25dc528292c466d320e257
fikipratama <107240699+fikipratama@users.noreply.github.com>
2022-06-24 09:48:06 +0700

    Update pondok-bambu.php

--- pondok-bambu.php
+++ pondok-bambu.php
@@ -246,6 +246,7 @@ Kelurahan Pondok Bambu memiliki Luas Wilayah 5.00 Km2, terdiri dari 13,337 Kelua
 							<li>Facebook: <a href="https://www.facebook.com/kelpondokbambu/">@kelpondokbambu</a> </li> 
 							<li>Twitter: <a href="https://mobile.twitter.com/kelbambu">@KelBambu</a> </li>	
 							<li>Youtube: <a href="https://www.youtube.com/channel/UCtHQojuTOpsE3z45pLyD0vA/null">@Kelurahan Pondok Bambu</a></li>
+								
 							</ul>
                         </div>
                        <!-- <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>-->
