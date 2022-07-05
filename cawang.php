b42dc7b67c75e684cf9981aea80c1841d3c0476a
alfarobee <93897173+alfarobee@users.noreply.github.com>
2022-07-05 10:39:28 +0700

    Update cawang.php

--- cawang.php
+++ cawang.php
@@ -146,7 +146,7 @@
 		    <h4 class="font-weight-light">Lurah</h4>
                     <h1 class="font-weight-light">Didik Diarjo SE, ME</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
-                     url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
+                     url('foto/foto-lurah-cawang') }}" alt="foto-lurah" /></p>
               </div>
                     <h4 class="font-weight-light">Kecamatan Kramat Jati</h4>
 					<h1 class="font-weight-light">Kelurahan Cawang </h1>
