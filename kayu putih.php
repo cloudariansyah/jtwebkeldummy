2e94d26822b96ba508a72b133aeb3cc1225ff37e
Arturobee <87790251+Arturobee@users.noreply.github.com>
2022-07-05 11:25:17 +0700

    Update kayu putih.php

--- kayu putih.php
+++ kayu putih.php
@@ -145,7 +145,7 @@
 		    <h4 class="font-weight-light">Lurah</h4>
                     <h1 class="font-weight-light">Artika Ristiana</h1>
                     <p><img class="img-fluid rounded mb-4 mb-lg-0" src="{{
-                     url('frontend/kelurahan/IMG-20220609-WA0002.jpg') }}" alt="foto-lurah" /></p>
+                     url('frontend/kelurahan/lurah_kayu_putih.jpg') }}" alt="foto-lurah" /></p>
               </div>
                     <h4 class="font-weight-light">Kecamatan Pulogadung </h4>
 					<h1 class="font-weight-light">Kelurahan Kayu Putih </h1>
